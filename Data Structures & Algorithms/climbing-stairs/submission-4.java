ee71706cd23e3c864af0e1108fa9fc6dac3b0d3c
Manish <mskasaudhan12@gmail.com>
2026-04-24 22:14:35 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        // int re = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(i>2){
+        //         break;
+        //     }else{
+        //         re = i+1;
+        //     }
+        // }
+        // return re;
+        int one = 1, two = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+
+        return one;
+    }
+}
